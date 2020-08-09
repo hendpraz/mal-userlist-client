209773cbba5ab0b20864014de929a8b893e7bf8e
hendpraz <mhendryp99@gmail.com>
2020-08-09 17:33:08 +0700

    Reintegrate with backend, update with username input

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,104 +1,137 @@
 import React, { useState } from "react";
-import { PageHeader, Table, Button } from "react-bootstrap";
+import { PageHeader, FormGroup, FormControl, Table, ControlLabel } from "react-bootstrap";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
+import axios from 'axios';
+import LoaderButton from "../components/LoaderButton";
 
 export default function Home() {
-  const [animeList, setAnimeList] = useState([]);
+  const [animeList, setAnimeList] = useState(null);
   const [userList, setUserList] = useState([]);
   const [userInput, setUserInput] = useState("");
-  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function loadAnimes() {
-    
+  function loadAnimes(arr) {
+    const baseUrl = "http://localhost:5000";
+    const path = "/anime-stats";
+    return axios({
+      method: 'post',
+      url: `${baseUrl+path}`,
+      data: {
+        username_list: arr
+      }
+    });
   }
 
-  function handleSubmitUsername(event) {
-    event.preventDefault();
-    try {
-      let arr = s.split(",").map(function(item) { return item.trim(); });
-      setUserList(arr);
+  function renderAverage(anime) {
+    let avg = 0;
+    let n = 0;
 
-      const myAnimeList = await loadAnimes(arr);
-      setAnimeList(myAnimeList);
-    } catch (e) {
-      onError(e);
+    for (let key in anime.rates) {
+      avg += anime.rates[key];
+      n += 1;
     }
+    avg /= n;
+    return avg.toFixed(2);
+  }
+
+  function renderRatings(anime) {
+    let arr = [];
+    for (let key in userList) {
+      const y = anime.rates[userList[key]] || '-';
+      arr.push(<td>{y}</td>);
+    }
+    let x = [{}].concat(arr);
+    x.shift();
+    return [x];
   }
 
   function renderAnimeList(animeList) {
-    let x = [{}].concat(animeList).map((anime, i) =>
-      i !== 0 &&
-        <tr>
-          <td>
-            {i}
-          </td>
-          <td>
-            {anime.anime_title}
-          </td>
-          {() => {
-            
-          }}
-          <td>
-            {anime.rates[userList[0]]}
-          </td>
-          <td>
-            {anime.rates[userList[1]]}
-          </td>
-          <td>
-            {anime.rates[userList[2]]}
-          </td>
-          <td>
-            {anime.rates[userList[3]]}
-          </td>
-          <td>
-            {anime.rates[userList[4]]}
-          </td>
-          <td>
-            {() => {
-              avg = 0;
-              n = 0;
-
-              for (key in anime.rates) {
-                avg += anime.rates[key];
-                n += 1;
-              }
-              avg /= n;
-              return avg.toFixed(2)
-            }}
-          </td>
-        </tr>
-    );
+    let arr = [];
+    let i = 0;
+    for (let key in animeList) {
+      const anime = animeList[key];
+      i += 1;
+      arr.push(<tr key={`row${i}`}>
+                  <td>
+                    {i}
+                  </td>
+                  <td>
+                    {anime.anime_title}
+                  </td>
+                  {renderRatings(anime)}
+                  <td>
+                  {renderAverage(anime)}
+                  </td>
+                </tr>);
+    }
+    let x = [{}].concat(arr);
     x.shift();
-    return x;
+    return [x];
   }
 
   function renderUserTableHead(userList) {
     let x = [{}].concat(userList).map((user, i) =>
     i !== 0 &&
-      <th>{user}</th>
+      <th key={`head-${i}`}>{user}</th>
     );
     x.shift();
     return x;
   }
 
+  function validateQueryForm() {
+    return userInput.length > 0;
+  }
+
+  async function handleQuerySubmit(event) {
+    event.preventDefault();
+    try {
+      setIsLoading(true);
+      let arr = userInput.split(",").map(function(item) { return item.trim(); });
+      setUserList(arr);
+
+      const myAnimeList = await loadAnimes(arr);
+      setAnimeList(myAnimeList.data);
+    } catch (e) {
+      onError(e);
+    }
+    setIsLoading(false);
+  }
+
   return (
     <div className="Home">
       <div className="animes">
-        <PageHeader>Rating Anime</PageHeader>
-        <Button color="secondary" onClick={handlePrevAnime}>Sebelumnya</Button>
-        <Button color="primary" onClick={handleNextAnime}>Berikutnya</Button>
+        <PageHeader>Anime User Ratings</PageHeader>
+        <FormGroup controlId="query" bsSize="large">
+          <ControlLabel>Usernames, separate with commas</ControlLabel>
+          <FormControl
+            autoFocus
+            type="tel"
+            onChange={e => setUserInput(e.target.value)}
+            placeholder="Example: username1, username2, username3"
+          />
+        </FormGroup>
+          <LoaderButton
+            block
+            type="submit"
+            bsSize="large"
+            isLoading={isLoading}
+            disabled={!validateQueryForm()}
+            onClick={handleQuerySubmit}
+          >
+            Submit Username
+          </LoaderButton>
         <Table>
           <thead>
             <tr>
-              <th>Nomor</th>
-              <th>Judul Anime</th>
+              <th>#</th>
+              <th>Title</th>
               { userList && renderUserTableHead(userList) }
               <th>Avg</th>
             </tr>
           </thead>
           <tbody>
-            {animeList && renderAnimeList(animeList)}
+            { animeList && renderAnimeList(animeList)}
           </tbody>
         </Table>
           

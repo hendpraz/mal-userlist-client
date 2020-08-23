d56c8d0e1c82443fbec62dd62a65e133e53cf7fe
Muhammad Hendry Prasetya <13517105@std.stei.itb.ac.id>
2020-08-23 23:16:25 +0700

    Bugfix: Clear animelist when loading

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -21,6 +21,7 @@ function Home(props) {
     async function onLoad() {
       try {
         setIsLoading(true);
+        setAnimeList(null);
         let query = props.location.search;
         if (query) {
           query = query.split("=");
